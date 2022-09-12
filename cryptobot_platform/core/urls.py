07454ee36ebc1bf1961a3d517e895f6f29ad66ed
bubikhonza <37843347+bubikhonza@users.noreply.github.com>
2022-09-12 23:04:50 +0200

    initial accounts things

---
+++ cryptobot_platform/core/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import TemplateView
+
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+]
